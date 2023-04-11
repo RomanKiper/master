3d03017061c58fb4d825996fda451ea9ab500539
romankiper <roman_kiper@mail.ru>
2023-04-11 23:03:46 +0300

    add postgresql correct gitignore add requirements

--- master/settings.py
+++ master/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = "master.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bogdan_site',
+        'USER': 'dev',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
